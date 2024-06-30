fc203c5a1074be35e76393e1e126622f1c65ee05
Dácio Braga <fdacio@gmail.com>
2024-06-29 23:29:39 -0300

    requisicao compra: migration e model

---
+++ app/RequisicaoCompra.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class RequisicaoCompra extends Model
+{
+    protected $table = 'requisicoes_compras';
+    protected $fillable = ['id_requisitante', 'id_solicitante', 'id_veiculo', 'data', 'tipo', 'autorizacao_cotacao', 'local_entrega'];
+}
---
+++ database/migrations/2024_06_30_021214_create_requisicoes_compras_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRequisicoesComprasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('requisicoes_compras', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('id_requisitante');
+            $table->unsignedBigInteger('id_solicitante');
+            $table->unsignedBigInteger('id_veiculo');
+            $table->date('data');
+            $table->enum('tipo', ['PRODUTO', 'SERVICO']);
+            $table->string('autorizacao_cotacao', 500)->nullable();
+            $table->string('local_entrega', 500)->nullable();
+            $table->foreign('id_requisitante')->references('id')->on('centros_custos');
+            $table->foreign('id_solicitante')->references('id')->on('solicitantes');
+            $table->foreign('id_veiculo')->references('id')->on('veiculos');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('requisicoes_compras');
+    }
+}
