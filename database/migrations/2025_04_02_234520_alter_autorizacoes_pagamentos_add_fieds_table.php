20341cbb66dfe8b479de1b0ca410af3181d464dc
fdacio <fdacio@gmail.com>
2025-04-02 21:22:37 -0300

    autorizacao de pagamentos: table novos campos e documentos

--- database/migrations/2025_04_02_234520_alter_autorizacoes_pagamentos_add_fieds_table.php
+++ database/migrations/2025_04_02_234520_alter_autorizacoes_pagamentos_add_fieds_table.php
@@ -14,7 +14,7 @@ class AlterAutorizacoesPagamentosAddFiedsTable extends Migration
     public function up()
     {
         Schema::table('autorizacoes_pagamentos', function (Blueprint $table) {
-            $table->unsignedBigInteger('id_usuario_autorizacao')->nullable()->after('chassi');
+            $table->unsignedBigInteger('id_usuario_autorizacao')->nullable()->after('chave_pix');
             $table->date('data_autorizacao')->nullable()->after('id_usuario_autorizacao');
             $table->boolean('paga')->default(false)->after('data_autorizacao');
             $table->date('data_pagamento')->nullable()->after('paga');
