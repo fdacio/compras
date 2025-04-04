0e91f9909d77693a9751c53d148cd88ebaa61f3a
fdacio <fdacio@gmail.com>
2025-04-04 16:00:16 -0300

    veiculo model: refatoracao

--- app/Veiculo.php
+++ app/Veiculo.php
@@ -7,7 +7,20 @@ use Illuminate\Database\Eloquent\Model;
 class Veiculo extends Model
 {
     protected $table = 'veiculos';
-    protected $fillable = ['id_frota', 'id_empresa', 'id_centro_custo', 'id_tipo_veiculo', 'marca', 'modelo', 'placa', 'uf', 'cor', 'ano', 'renavan', 'chassi'];
+    protected $fillable = [
+        'id_frota', 
+        'id_empresa', 
+        'id_centro_custo', 
+        'id_tipo_veiculo', 
+        'marca', 
+        'modelo', 
+        'placa', 
+        'uf', 
+        'cor', 
+        'ano', 
+        'renavan', 
+        'chassi'
+    ];
 
     public function empresa()
     {
