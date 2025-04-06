e05a34f1491ed787ede8b3b1db7d0eeae6a1e45a
fdacio <fdacio@gmail.com>
2025-04-05 23:29:49 -0300

    correcao da requisição de compra

--- app/Reports/DemoRequisicaoCompraPdf.php
+++ app/Reports/DemoRequisicaoCompraPdf.php
@@ -82,11 +82,14 @@ class DemoRequisicaoCompraPdf extends ReportPdf
 
         $this->SetFont('Arial', 'B', 8);
         $this->Cell(20, 5, utf8_decode('Empresa:'), 'LTR');
+        $this->SetFont('Arial', '', 8);
         $this->Cell(80, 5, utf8_decode($requisicao->veiculo->empresa->pessoa->nomeRazaoSocial), 'LTR');
         $this->Ln();
-        
+
+        $this->SetFont('Arial', 'B', 10);
         $this->SetXY($x + 100, $y - 5);
         $this->MultiCell(90, 30, utf8_decode('URGENTE'), 'LTRB', 'L');
+
 
         $this->SetFont('Arial', 'B', 8);
         $this->Cell(190, 5, utf8_decode('Itens da requisição'), 1, 0, 'C');
