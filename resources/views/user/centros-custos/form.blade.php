48504fcd34d44a5963691c8570eb68869ee5e289
fdacio <fdacio@gmail.com>
2025-04-11 20:32:48 -0300

    users centros de custos

--- resources/views/user/centros-custos/form.blade.php
+++ resources/views/user/centros-custos/form.blade.php
@@ -2,7 +2,8 @@
     @foreach ($centrosCustos as $centroCusto)
     <tr>
         <td> 
-            <input type="checkbox" value="{{ $centroCusto->id }}" name="centros_custos[]" id="{{ $centroCusto->id . '_' . 'CC' }}" {{ ($centrosCustosUser->where('id_centro_custo', $centroCusto->id)->get()->count() > 0) ? 'checked' : '' }}>
+            <input type="checkbox" value="{{ $centroCusto->id }}" name="centros_custos[]" id="{{ $centroCusto->id . '_' . 'CC' }}" 
+            {{ ($centrosCustosUser->where('id_centro_custo', $centroCusto->id)->get()) ? 'checked' : '' }}>
             <label for="{{ $centroCusto->id . '_' . 'CC' }}">{{ $centroCusto->nome }}</label>
         </td>
     </tr>
