b1d50a90a936e1877f1b7a8f68f9b2fbeddf0a23
fdacio <fdacio@gmail.com>
2025-04-11 19:49:40 -0300

    users centros de custos

--- app/User.php
+++ app/User.php
@@ -48,7 +48,7 @@ class User extends Authenticatable
 
     public function centrosCustos() 
     {
-        return $this->belongsToMany(UserCentroCusto::class, 'id_user', 'id');
+        return $this->belongsToMany(UserCentroCusto::class);
     }
 
 }
