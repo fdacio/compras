506b9fcb17e680aeb8ecdd51b971bcc3d299fc2f
Dácio Braga <fdacio@gmail.com>
2025-04-25 00:44:32 -0300

    processo de contação de requisição de compras

--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,7 @@ Route::get('cep/{numero}', 'Api\CepController@getDados')->name('api.cep.show');
 Route::get('cnpj/{cnpj}', 'Api\PessoasJuridicasController@getDados')->name('api.pessoasjuridicas.cnpj');
 Route::get('cpf/{cpf}', 'Api\PessoasFisicasController@getDados')->name('api.pessoasfisicas.cpf');
 Route::get('empresas/{cnpjcpf}', 'Api\EmpresasController@empresa')->name('api.empresas.cnpjcpf');
-Route::get('empresas', 'Api\EmpreasController@empresas')->name('api.empresas');
+Route::get('empresas', 'Api\EmpresasController@empresas')->name('api.empresas');
 Route::post('unidades/store', 'Api\UnidadesController@store')->name('api.unidades.store');
 Route::get('favorecidos/{cnpjcpf}', 'Api\FavorecidosController@favorecido')->name('api.favorecidos.cnpjcpf');
 Route::get('favorecidos', 'Api\FavorecidosController@favorecidos')->name('api.favorecidos');
