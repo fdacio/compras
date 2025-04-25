5c5512f5ff78371d111824e6da5707edef2f8e54
Dácio Braga <fdacio@gmail.com>
2025-04-25 11:51:17 -0300

    requisicoes compras: autorizacoes

--- routes/web.php
+++ routes/web.php
@@ -65,9 +65,8 @@ Route::group(['middleware' => ['auth', 'auth.access', 'check.permissao']], funct
     Route::put('requisicoes-compras/{requisicao}/cotar', 'RequisicoesComprasController@cotar')->name('requisicoes-compras.cotar');
     Route::get('requisicoes-compras/autorizacao', 'RequisicoesComprasController@autorizacao')->name('requisicoes-compras.autorizacao');
    
-    Route::resource('cotacoes', 'CotacoesController', ['parameters' => ['cotacaoes' => 'cotacao']]);
     Route::get('cotacoes', 'CotacoesController@index')->name('cotacoes.index');
-    Route::get('cotacoes/{cotacao}/edit', 'CotacoesController@edit')->name('cotacoes.index');
+    Route::get('cotacoes/{cotacao}/edit', 'CotacoesController@edit', ['parameters' => ['cotacaoes' => 'cotacao']])->name('cotacoes.index');
 
     Route::resource('autorizacoes-pagamentos', 'AutorizacoesPagamentosController', ['parameters' => ['autorizacoes-pagamentos' => 'autorizacao']]);
     Route::get('autorizacoes-pagamentos/item/create/{autorizacao}', 'AutorizacoesPagamentosController@itemCreate')->name('autorizacoes-pagamentos.item.create');
