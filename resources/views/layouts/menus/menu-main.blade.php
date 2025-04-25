1456b8f027cc7a333b89d7ca66a70705c1d8852c
Dácio Braga <fdacio@gmail.com>
2025-04-24 23:24:29 -0300

    processo de contação de requisição de compras

--- resources/views/layouts/menus/menu-main.blade.php
+++ resources/views/layouts/menus/menu-main.blade.php
@@ -12,7 +12,7 @@
             class="collapsed nav-link">
             <i class="fa fa-edit"></i> <span class="nav-label">Cadastros Gerais</span> <span
                 class="fa fa-chevron-left pull-right"></span> </a>
-        <ul class="navbar-nav sub-menu collapse  p-1" id="cadastros-gerais">
+        <ul class="navbar-nav sub-menu collapse p-1" id="cadastros-gerais">
             <li class="nav-item"><a href="{{ route('centros-custos.index') }}" class="nav-link">Centros de Custos</a>
             </li>
             <li class="nav-item"><a href="{{ route('solicitantes.index') }}" class="nav-link">Solicitantes</a></li>
@@ -29,7 +29,7 @@
             class="collapsed nav-link">
             <i class="fa fa-edit"></i> <span class="nav-label">Requisições de Compras</span> <span
                 class="fa fa-chevron-left pull-right"></span> </a>
-        <ul class="navbar-nav sub-menu collapse  p-1" id="cadastros-gerais">
+        <ul class="navbar-nav sub-menu collapse p-1" id="requisicoes-compras">
 
             <li class="nav-item"> <a href="{{ route('requisicoes-compras.index') }}" class="nav-link"><i
                         class="fa fa-search"></i> <span class="nav-label">Consultar</span></a></li>
