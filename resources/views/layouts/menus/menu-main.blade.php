788fea0a1b95f79c71b6f06b99016201a991c8ed
Dácio Braga <fdacio@gmail.com>
2025-04-25 00:11:26 -0300

    processo de contação de requisição de compras

--- resources/views/layouts/menus/menu-main.blade.php
+++ resources/views/layouts/menus/menu-main.blade.php
@@ -13,8 +13,8 @@
             <i class="fa fa-edit"></i> <span class="nav-label">Cadastros Gerais</span> <span
                 class="fa fa-chevron-left pull-right"></span> </a>
         <ul class="navbar-nav sub-menu collapse p-1" id="cadastros-gerais">
-            <li class="nav-item"><a href="{{ route('centros-custos.index') }}" class="nav-link">Centros de Custos</a>
-            </li>
+            <li class="nav-item ml-2"><a href="{{ route('centros-custos.index') }}" class="nav-link">Centros de
+                    Custos</a></li>
             <li class="nav-item ml-2"><a href="{{ route('solicitantes.index') }}" class="nav-link">Solicitantes</a></li>
             <li class="nav-item ml-2"><a href="{{ route('empresas.index') }}" class="nav-link">Empresas</a></li>
             <li class="nav-item ml-2"><a href="{{ route('fornecedores.index') }}" class="nav-link">Fornecedores</a></li>
@@ -27,24 +27,23 @@
 
     <li class="nav-item"> <a href="#" data-toggle="collapse" data-target="#requisicoes-compras"
             class="collapsed nav-link">
-            <i class="fa fa-edit"></i> <span class="nav-label">Requisições de Compras</span> <span
-                class="fa fa-chevron-left pull-right"></span> </a>
+            <i class="fa fa-edit"></i> <span class="nav-label">Requisições de Compras</span>
+            <span class="fa fa-chevron-left pull-right"></span> </a>
         <ul class="navbar-nav sub-menu collapse p-1" id="requisicoes-compras">
 
             <li class="nav-item ml-2"> <a href="{{ route('requisicoes-compras.index') }}" class="nav-link"><i
                         class="fa fa-search"></i> <span class="nav-label">Consultar</span></a></li>
 
             <li class="nav-item ml-2"> <a href="{{ route('requisicoes-compras.cotacoes') }}" class="nav-link"><i
-                class="fa fa-file-text-o"></i> <span class="nav-label">Cotações</span></a></li>
+                        class="fa fa-file-text-o"></i> <span class="nav-label">Cotações</span></a></li>
 
             <li class="nav-item ml-2"> <a href="{{ route('requisicoes-compras.autorizacoes') }}" class="nav-link"><i
-                    class="fa fa-check"></i> <span class="nav-label">Autorizações</span></a></li>
+                        class="fa fa-check"></i> <span class="nav-label">Autorizações</span></a></li>
         </ul>
     </li>
 
     <li class="nav-item"> <a href="{{ route('autorizacoes-pagamentos.index') }}" class="nav-link"><i
                 class="fa fa-calendar-check-o"></i> <span class="nav-label">Autorizações de Pagamentos</span></a></li>
-
 
     <li class="nav-item"> <a href="#" data-toggle="collapse" data-target="#e-commerce"
             class="collapsed nav-link"><i class="fa fa-file-pdf-o"></i> <span class="nav-label">Relatórios</span><span
@@ -61,7 +60,8 @@
     <ul class="navbar-nav sub-menu collapse p-1" id="users">
         <li><a href="{{ route('tipos-usuarios.index') }}" class="nav-link"><span
                     class="fa fa-link mr-1"></span>Tipos</a></li>
-        <li><a href="{{ route('user.index') }}" class="nav-link"><span class="fa fa-link mr-1"></span>Usuários</a></li>
+        <li><a href="{{ route('user.index') }}" class="nav-link"><span class="fa fa-link mr-1"></span>Usuários</a>
+        </li>
     </ul>
 
 </ul>
