8efc03da5285471acc9574f02ffc23adaadc4d77
Dácio Braga <fdacio@gmail.com>
2025-05-01 02:35:42 -0300

    cotacao editar

--- app/Http/Controllers/CotacoesController.php
+++ app/Http/Controllers/CotacoesController.php
@@ -55,6 +55,7 @@ class CotacoesController extends Controller
         try {
             DB::beginTransaction();
             $cotacao->requisicao->update(['situacao' => RequisicaoCompra::SITUACAO_PENDENTE]);
+            dd($cotacao->fornecedores);
             $cotacao->fornecedores->itens->delete();
             $cotacao->fornecedores->delete();
             $cotacao->delete();
