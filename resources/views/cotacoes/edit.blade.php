80a2c25e20d20c63a574b80e1bc875540babfa63
Dácio Braga <fdacio@gmail.com>
2025-05-01 00:47:10 -0300

    cotacao editar

--- resources/views/cotacoes/edit.blade.php
+++ resources/views/cotacoes/edit.blade.php
@@ -118,7 +118,7 @@
             ]) !!}
             <div class="form mt-2">
                 <div class="row">
-                    <div class="col-xs-7 col-sm-7 col-md-7">
+                    <div class="col-xs-10 col-sm-10 col-md-10">
                         <div class="form-group">
                             <label for="fornecedor">Fornecedor</label>
                             {!! Form::select(
@@ -134,7 +134,7 @@
 
                         </div>
                     </div>
-                    <div class="col-xs-5 col-sm-5 col-md-5">
+                    <div class="col-xs-2 col-sm-2 col-md-2">
                         <div class="form-group">
                             <label>&nbsp;</label>
                             {!! Form::button('<i class="fa fa-plus mr-2"></i> Adicionar', [
