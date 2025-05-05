37fa0a7751430c2b8f704a9616ca80591e3b3aaa
Dácio Braga <fdacio@gmail.com>
2025-05-05 19:03:33 -0300

    cotacao

--- resources/views/cotacoes/edit.blade.php
+++ resources/views/cotacoes/edit.blade.php
@@ -19,6 +19,18 @@
                     {{ session('danger') }}
                 </div>
             @endif
+            @if (count($errors) > 0)
+            <div class="alert alert-danger alert-dismissable ''">
+                <button type="button" class="close" data-dismiss="alert" aria-label="Fechar"><span aria-hidden="true">&times;</span></button>
+                <strong>Ops!</strong> Verifique os erros.<br><br>
+                <ul>
+                    @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+            @endif
+    
         </div>
         <div class="card-body">
 
