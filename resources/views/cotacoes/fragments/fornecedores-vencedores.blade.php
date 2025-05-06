6f1b166e72bdee12f004f61b276d07e393c0882f
Dácio Braga <fdacio@gmail.com>
2025-05-05 21:58:18 -0300

    cotacao

--- resources/views/cotacoes/fragments/fornecedores-vencedores.blade.php
+++ resources/views/cotacoes/fragments/fornecedores-vencedores.blade.php
@@ -23,7 +23,7 @@
                             </tr>
                         </thead>
                         <tbody>
-                            @foreach ($cotacao->fornecedores->where('id_fornecedor', $fornecedor->id_fornecedor)->itens as $i)
+                            @foreach ($cotacao->fornecedores->where('id_fornecedor', $fornecedor->id_fornecedor)->itens() as $i)
                             {{ $i }}
                                 <tr class="row-itens-cotacao">
                                     <td class="text-left text-sm">
