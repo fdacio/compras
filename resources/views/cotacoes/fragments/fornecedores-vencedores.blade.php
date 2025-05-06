d9bbc6bf1d23c6d929927bd903b81ce308924b69
Dácio Braga <fdacio@gmail.com>
2025-05-05 21:31:58 -0300

    cotacao

--- resources/views/cotacoes/fragments/fornecedores-vencedores.blade.php
+++ resources/views/cotacoes/fragments/fornecedores-vencedores.blade.php
@@ -1,20 +1,10 @@
 <div class="card">
     <div class="card-body">
-        <table class="table table-sm table-bordered">
-            <thead>
-                <tr>
-                    <th class="text-left text-sm">Fonecedor</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($cotacao->fornecedoresVencedores as $item)
-                    <tr class="row-itens-cotacao">
-                        <td class="text-left text-sm">
-                            {{ $item->fornecedor->pessoa->nome_razao_social }}
-                        </td>
-                    </tr>    
-                @endforeach
-            </tbody>
-        </table>
+        @foreach ($cotacao->fornecedoresVencedores as $item)
+            <div class="alert alert-success">
+                <strong>Fornecedor Vencedor:</strong>
+                {{ $item->fornecedor->pessoa->nome_razao_social }}
+            </div>
+        @endforeach
     </div>
 </div>
